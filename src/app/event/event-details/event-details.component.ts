5336f258e279b8e50a7948d8d24284b9bf55e4b5
Michael Tolfo <mtolfo@hchb.com>
2019-01-07 12:10:23 -0500

    using ActivatedRoute service

--- src/app/event/event-details/event-details.component.ts
+++ src/app/event/event-details/event-details.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from 'src/app/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styles: [`.container { padding-left:20px; padding-right: 20px;}
-  .event-image: {height: 100px; }`]
+  .event-image {height: 100px; }`]
 })
 export class EventDetailsComponent implements OnInit {
 
   event: any;
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(1);
+    this.event = this.eventService.getEvent(
+      +this.route.snapshot.params['id']
+    );
   }
 
 }
