9df919556db84fdecb8cd2e10cb2f5088b2fd9f0
Michael Tolfo <mtolfo@hchb.com>
2019-01-14 22:06:45 -0500

    From scratch create react model based form with validations.

--- src/app/event/event-details/create-session.component.ts
+++ src/app/event/event-details/create-session.component.ts
@@ -1,11 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ISession } from '../shared';
 
 @Component({
-  templateUrl: './create-session.component.html'
+  templateUrl: './create-session.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+      .error input {
+        background-color: #e3c3c5;
+      }
+      .error ::-webkit-input-placeholder {
+        color: #999;
+      }
+      .error ::-moz-placeholder {
+        color: #999;
+      }
+      .error :-moz-placeholder {
+        color: #999;
+      }
+      .error :ms-input-placeholder {
+        color: #999;
+      }
+    `
+  ]
 })
-
 export class CreateSessionComponent implements OnInit {
-  constructor() { }
+  constructor() {}
+  public newSessionForm: FormGroup;
+  public name: FormControl;
+  public presenter: FormControl;
+  public duration: FormControl;
+  public level: FormControl;
+  public abstract: FormControl;
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.name = new FormControl('', Validators.required);
+    this.presenter = new FormControl('', Validators.required);
+    this.duration = new FormControl('', Validators.required);
+    this.level = new FormControl('', Validators.required);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+
+    this.newSessionForm = new FormGroup({
+      name: this.name,
+      presenter: this.presenter,
+      duration: this.duration,
+      level: this.level,
+      abstract: this.abstract
+    });
+  }
+
+  saveSession(formValues: any) {
+    const session: ISession = {
+      id: undefined,
+      name: formValues.name,
+      presenter: formValues.presenter,
+      duration: +formValues.duration,
+      level: formValues.level,
+      abstract: formValues.abstract,
+      voters: []
+    };
+    console.log(session);
+  }
 }
